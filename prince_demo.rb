81529c3a6c85459b3c8ad6900b21f7a815923677
Steven Wilkin <stevenwilkin@gmail.com>
2015-03-09 15:57:31 +0000

    Add testing content

--- prince_demo.rb
+++ prince_demo.rb
@@ -5,6 +5,6 @@ class PrinceDemo < Sinatra::Base
   get '/' do
     content_type 'application/pdf'
     princely = Princely.new
-    princely.pdf_from_string('<h1>PrinceXML</h1>')
+    princely.pdf_from_string(File.read('index.html'))
   end
 end
